5b49c83e69a33ba2bd80479074aeb6d3dcf0e7ec
sngtmth <sngtmth@gmail.com>
2021-10-03 02:03:35 +0530

    Added: Total Working Day or hour for Month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -11,17 +11,20 @@ public class EmployeeWage {
 	public static final int IS_Part_Time = 2;
 	public static final int EMP_RATE_PER_HR = 20;
 	public static final int Num_Of_Working_Days = 20;
+	public static final int MAX_HR_IN_MONTH = 100;
 
 	// Driver Code
 	public static void main(String args[]) {
 		int empHr = 0;
 		int empWage = 0;
-		int day;
 		int total_EmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHr = 0;
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		// Using for loop we have calculate total Employee Wage for month.
-		// Assume Number of Working days is 20
-		for (day = 1; day < Num_Of_Working_Days; day++) {
+		// while condition use of total working hours or days is reached for a month
+		// Assume 100 hours and 20 days
+		while (totalEmpHr <= MAX_HR_IN_MONTH && totalWorkingDays < Num_Of_Working_Days) {
+			totalWorkingDays++;
 			// use random function gives value 0 or 1
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 			// print random values
@@ -49,7 +52,9 @@ public class EmployeeWage {
 			empWage = empHr * EMP_RATE_PER_HR;
 			// calculate total employee wages for month
 			total_EmpWage = total_EmpWage + empWage;
-			// print employee Wage
+			// calculate total Employee hour
+			totalEmpHr = totalEmpHr + empHr;
+			System.out.println("days" + " " + totalWorkingDays + " " + "emphrs" + " " + empHr);
 			System.out.println("Employee Wage=>" + empWage);
 			System.out.println();
 
