5acd74c3607c4ded9c0c1458327fe8c8abc2f685
sngtmth <sngtmth@gmail.com>
2021-10-03 01:27:12 +0530

    Added:Welcome Message Employee Wage Computation Program

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+}
