f7b0b9c0b45be279a386e0671f1c7baeb86c0077
sngtmth <sngtmth@gmail.com>
2021-10-03 01:33:20 +0530

    Added:Checking Employee Present or Absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,6 +5,15 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		// use random function gives value 0 or 1
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		// print random values
+		System.out.println("Random values=>" + empCheck);
+		if (empCheck == 1) {
+			System.out.println("Employee Present");
+		} else {
+			System.out.println("Employee Absent");
+		}
 	}
 
 }
