81e89d13d4455d9b70c94636e8b71da66003cc20
sngtmth <sngtmth@gmail.com>
2021-10-03 12:21:49 +0530

    Added:Refactor Code to write class and method to calculate employee wage Problem

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,25 +1,27 @@
 package com.bridgelabz;
 
 /**
- * Employee Wage Computation Problem
+ * Refactoring the Code to write a Class Method to Compute Employee Wage - Use
+ * Class Method and Class
  * 
  * @author CHANDRU
  *
  */
-public class EmployeeWage {
+class EmpWage {
+	// declaration of variables
+	int empHr = 0;
+	int empWage = 0;
+	int totalEmpWage = 0;
+	int totalWorkingDays = 0;
+	int totalEmpHr = 0;
+
 	public static final int IS_Full_TIME = 1;
 	public static final int IS_Part_Time = 2;
 	public static final int EMP_RATE_PER_HR = 20;
 	public static final int Num_Of_Working_Days = 20;
 	public static final int MAX_HR_IN_MONTH = 100;
 
-	// Driver Code
-	public static void main(String args[]) {
-		int empHr = 0;
-		int empWage = 0;
-		int total_EmpWage = 0;
-		int totalWorkingDays = 0;
-		int totalEmpHr = 0;
+	public void calculateWage() {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		// while condition use of total working hours or days is reached for a month
 		// Assume 100 hours and 20 days
@@ -51,7 +53,7 @@ public class EmployeeWage {
 			// calculate employee Wage
 			empWage = empHr * EMP_RATE_PER_HR;
 			// calculate total employee wages for month
-			total_EmpWage = total_EmpWage + empWage;
+			totalEmpWage = totalEmpWage + empWage;
 			// calculate total Employee hour
 			totalEmpHr = totalEmpHr + empHr;
 			System.out.println("days" + " " + totalWorkingDays + " " + "emphrs" + " " + empHr);
@@ -60,6 +62,19 @@ public class EmployeeWage {
 
 		}
 		// Print Total Employee Wages
-		System.out.println("Total Employee Wages for Month=>" + total_EmpWage);
+		System.out.println("Total hour=>" + totalEmpHr);
+		System.out.println("Total Employee Wages for Month=>" + totalEmpWage);
+	}
+
+}
+
+public class EmployeeWage {
+
+	// Driver Code
+	public static void main(String args[]) {
+		EmpWage emp = new EmpWage();
+
+		emp.calculateWage();
+
 	}
 }
